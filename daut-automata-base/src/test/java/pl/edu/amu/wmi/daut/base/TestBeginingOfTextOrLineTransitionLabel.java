1a55a6c75f6bb28df586b75d93d114bdc78df53d
Ela <Ela@.(none)>
2012-01-24 00:47:59 +0100

    htak

--- daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestBeginingOfTextOrLineTransitionLabel.java
+++ daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestBeginingOfTextOrLineTransitionLabel.java
@@ -45,7 +45,7 @@ public class TestBeginingOfTextOrLineTransitionLabel extends TestCase {
         //pozycja==0. dziala
         assertTrue(t.doCheckContext("pampararam", 0));
         //na poczatku wiersza
-        assertTrue(t.doCheckContext("ja\nmhm", 4));
+        assertFalse(t.doCheckContext("ja\nmhm", 4));
         //w srodku wiersza
         //assertTrue(t.doCheckContext("baklazan", 5));
         //assertFalse(t.doCheckContext("srutu\ntutu", 5));
